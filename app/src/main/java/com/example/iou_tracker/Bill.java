f12c6c55583d05f8e22a140cfed327cf7fefb30b
Pulkit Vajpayee <pulkitvajpayee07@gmail.com>
2020-04-12 13:44:51 +0530

    "bill split"

---
+++ app/src/main/java/com/example/iou_tracker/Bill.java
@@ -0,0 +1,34 @@
+package com.example.iou_tracker;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Bill {
+    public String billNo;
+    public HashMap<String, Double> listOfPerson = new HashMap<>();
+    public Double amount;
+
+
+    public Bill() {
+
+    }
+
+    public Bill(String billNo, HashMap<String, Double> map , Double amount) {
+        this.billNo = billNo;
+        this.amount = amount;
+        this.listOfPerson = map;
+
+    }
+
+    public String getBillNo() {
+        return billNo;
+    }
+
+    public HashMap<String, Double> getListOfPerson() {
+        return listOfPerson;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
--- app/src/main/java/com/example/iou_tracker/SplitBillActivity.java
+++ app/src/main/java/com/example/iou_tracker/SplitBillActivity.java
@@ -1,15 +1,27 @@
 package com.example.iou_tracker;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.media.MediaDrm;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -17,14 +29,26 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import io.opencensus.tags.Tag;
 
 public class SplitBillActivity extends AppCompatActivity {
 
     ArrayList<String> selectedItems = new ArrayList<>();
     ListView listView;
+    Button btSplit;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     List<String> nameList = new ArrayList();
+    EditText amount;
+    double amount1;
+    String gName;
+    CollectionReference billRef = db.collection("Bill");
 
 
     @Override
@@ -33,11 +57,13 @@ public class SplitBillActivity extends AppCompatActivity {
         setContentView(R.layout.activity_split_bill);
 
         Bundle bundle = getIntent().getExtras();
-        String gName = bundle.getString("gName");
+        gName = bundle.getString("gName");
         Toast.makeText(SplitBillActivity.this,gName,Toast.LENGTH_SHORT).show();
 
         listView = findViewById(R.id.listPerson);
         listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        amount = findViewById(R.id.editText7);
+        btSplit = findViewById(R.id.button12);
 
         db.collection("Group").whereEqualTo("gName",gName)
                 .addSnapshotListener(new EventListener<QuerySnapshot>() {
@@ -69,6 +95,64 @@ public class SplitBillActivity extends AppCompatActivity {
                 });
             }
         });
+
+        btSplit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String s = amount.getText().toString();
+                if(s.trim().equals("")){
+                    Toast.makeText(SplitBillActivity.this,"Yeha aaya ",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    try {
+                        amount1 = Double.parseDouble(s);
+                    } catch (Exception e) {
+                        Toast.makeText(SplitBillActivity.this, "Yeha pr aaya toh hai ", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                Toast.makeText(SplitBillActivity.this,"Amount ="+amount.getText().toString(),Toast.LENGTH_SHORT).show();
+
+                splitBill();
+            }
+        });
+
+    }
+    public void splitBill(){
+        final HashMap<String , Double> map = new HashMap<>();
+        for(int i =0;i<selectedItems.size();i++){
+            map.put(selectedItems.get(i),amount1/selectedItems.size());
+        }
+//        db.collection("Bill").whereEqualTo("billNo",gName)
+//                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+//                    @SuppressLint("NewApi")
+//                    @Override
+//                    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+//                        if(!queryDocumentSnapshots.isEmpty()) {
+////                            HashMap<String, Double> map1 = new HashMap<>();
+////                            for (DocumentSnapshot snapshot : queryDocumentSnapshots) {
+////                                map1 = (HashMap<String, Double>) snapshot.get("listOfPerson");
+////                            }
+//
+//
+//
+//
+//                            db.collection("Bill").document(gName).update("listOfPerson",map);
+//                            db.collection("Bill").document(gName).update("Amount",amount1);
+//                            Intent toBack = new Intent(SplitBillActivity.this,HomeActivity.class);
+//                            startActivity(toBack);
+//                        }
+//                        else{
+                            Bill bill = new Bill(gName,map,amount1);
+                            System.out.println(bill.billNo);
+                            billRef.document(gName).set(bill);
+                            Intent toBack = new Intent(SplitBillActivity.this,HomeActivity.class);
+                            startActivity(toBack);
+//                        }
+//                    }
+//                });
+
 
     }
 }
